0d61cee0bd877f4e136f2da84000ca71535a2e6c
tuckerr8 <rodzell@outlook.com>
2024-02-18 15:55:39 -0500

    update 2

--- src/CtoFConverter.java
+++ src/CtoFConverter.java
@@ -4,24 +4,34 @@ public class CtoFConverter {
     public static void main(String[] args) {
         double celsius;
         double fahrenheit;
-        String trash;
+        String trash = "";
+        boolean done = false;
 
         // fahrenheit = (celsius * 9/5) + 32
 
-        Scanner in = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter the temp for Celsius: ");
+        do {
+            System.out.println("Enter the temp for Celsius: ");
 
-        if (in.hasNextDouble()) {
-            celsius = in.nextDouble();
-            in.nextLine();
+            if (scanner.hasNextDouble()) {
+                celsius = scanner.nextDouble();
+                scanner.nextLine();
 
-            fahrenheit = ((celsius * 1.8) + 32);
+                if(celsius >=0 || celsius <=0){
+                    fahrenheit = ((celsius * 1.8) + 32);
+                    System.out.printf("The temp in Fahrenheit is: " + fahrenheit);
+                    done = true;
+                }else {
+                    System.out.printf("You must enter a correct value.");
+                    scanner.nextLine();
+                }
+            } else {
+                trash = scanner.nextLine();
+                System.out.println("You entered an invalid value: " + trash);
+            }
 
-            System.out.printf("The temp in Fahrenheit is: " + fahrenheit);
+        }while (!done);
 
-        } else {
-            System.out.println("You entered an invalid value");
-        }
     }
 }
---
+++ src/FuelCosts.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class FuelCosts {
+    public static void main(String[] args) {
+        double gallonsOfGas = 0;
+        double fuelEfficiency = 0;
+        double pricePerGallon = 0;
+        boolean done = false;
+
+        Scanner scanner = new Scanner(System.in);
+
+        do{
+            System.out.println("Enter the gallons in the tank: ");
+
+            if(scanner.hasNextDouble()){
+                gallonsOfGas = scanner.nextDouble();
+            }else {
+                if(gallonsOfGas < 0){
+                    System.out.println("Please enter a correct value");
+                }
+            }
+
+            System.out.println("Enter the fuel efficiency in miles per gallon: ");
+
+            if(scanner.hasNextDouble()){
+                fuelEfficiency = scanner.nextDouble();
+            }else{
+                if(fuelEfficiency < 0){
+                    System.out.println("Please enter a correct value");
+                }
+            }
+
+            System.out.println("Enter the price per gallon: ");
+
+            if(scanner.hasNextDouble()){
+                pricePerGallon = scanner.nextDouble();
+                done = true;
+            }else{
+                if(pricePerGallon < 0){
+                    System.out.println("Please enter a correct value");
+                }
+            }
+        }while(!done);
+
+        double costPer100Miles = (100 / fuelEfficiency) * pricePerGallon;
+        double milesWithGas = gallonsOfGas * fuelEfficiency;
+
+        System.out.println("The cost per 100 miles is $" + costPer100Miles);
+        System.out.println("Your vehicle can go " + milesWithGas + " miles with the gas in your tank.");
+
+    }
+}
